31cf70042fe3b8e53b091a528beb68d45d16243c
= <=>
2019-05-14 15:00:30 -0300

    Criação do Pacote dominio e da classe Cliente

---
+++ src/com/prjcadcliente/dominio/Cliente.java
@@ -0,0 +1,11 @@
+package com.prjcadcliente.dominio;
+
+public class Cliente {
+	
+	private int id;
+	private String nome;
+	private String email;
+	private String telefone;
+	private int idade;
+
+}
