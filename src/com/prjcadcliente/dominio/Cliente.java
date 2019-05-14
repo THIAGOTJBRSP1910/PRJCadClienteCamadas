a8278c24c785769290e203bac9255562b88dc3d5
= <=>
2019-05-14 15:25:38 -0300

    Criação do Pacote Persistencia e da Classe CRUDCliente com os metodos sem corpo

--- src/com/prjcadcliente/dominio/Cliente.java
+++ src/com/prjcadcliente/dominio/Cliente.java
@@ -7,5 +7,57 @@ public class Cliente {
 	private String email;
 	private String telefone;
 	private int idade;
+	
+	public Cliente() {
+	}
+
+	public Cliente(int id, String nome, String email, String telefone, int idade) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.idade = idade;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
 
 }
